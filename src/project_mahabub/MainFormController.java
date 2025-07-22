e1c208ef7ebc515091a4f6312d28154c03858fe7
MD MAHABUBUR RAHMAN <rahmanmdmahabubur666@gmail.com>
2025-07-22 19:43:52 +0600

    dashboard disgine by css

---
+++ src/project_mahabub/MainFormController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package project_mahabub;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author MD Mahabubur Rahman
+ */
+public class MainFormController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
